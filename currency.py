abe6e117861ba39237c780da79518ef5908573b6
stanislavsabev <bezraboten.34@gmail.com>
2022-11-17 13:13:24 +0100

    My changes

--- currency.py
+++ currency.py
@@ -1,43 +1,85 @@
+import traceback
 import xml.etree.ElementTree as ET
+from typing import Dict, List, Optional
+
 import requests
 
-def getxml(url):
-    resp = requests.get(url)
-    root = ET.fromstring(resp.text)
-    return root
 
-def convert(amount, toCurrency):
-    url = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml'
-    root = getxml(url)
-    amountConverted = -1
-    for i, child in enumerate(root.iter('*')):
-        if i >= 6:
-            if child.attrib['currency'] == toCurrency:
-                rate = child.attrib['rate']
-                amountConverted = float(rate) * amount
-    if amountConverted == -1:
-        return False
-    return amountConverted
-def getListCurrency():
-    url = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml'
-    root = getxml(url)
-    listCurrency = set()
-    for i, child in enumerate(root.iter('*')):
-        if i >= 6:
-            listCurrency.add(child.attrib['currency'])
-    return listCurrency
+class CurrencyRepo:
+    def __init__(self, url) -> None:
+        self._url = url
+        self._data: Dict[str, float] = {}
+        self._eoh = 6
+        self._timeout = 15
+
+    def _getXml(self):
+        resp = requests.get(url=self._url, timeout=self._timeout)
+        if not resp.ok:
+            resp.raise_for_status()
+        return ET.fromstring(resp.text)
+
+    def getCurrencies(self) -> Dict[str, float]:
+        if not self._data:
+            root = list(self._getXml().iter("*"))
+            for i, child in enumerate(root):
+                if i < self._eoh:
+                    continue
+                currency = child.attrib["currency"]
+                rate = float(child.attrib["rate"])
+                self._data[currency] = rate
+        return self._data
+
+
+def printAvailableCurrencies(currencyList: List[str]):
+    print("List of currencies convertable", end="\n\n")
+    print(" ".join(currencyList), end="\n\n")
+
+
+def getUserAmount() -> Optional[int]:
+    amount = None
+    try:
+        amount = int(input("Enter the amount to convert (whole number > 0) : "))
+    except ValueError:
+        print("Not a valid number")
+        return None
+    if amount < 0:
+        print("Number must be > 0")
+        return None
+    return amount
+
+
+def getUserCurrency(availableCurrencies: set) -> Optional[str]:
+    currency = str(input("To which currency? (3 letters) : ")).upper()
+    if currency not in availableCurrencies:
+        print("Invalid currency code.")
+        return None
+    return currency
+
+
+def convert(amount: int, toCurrency: str, currencyData: Dict[str, float]):
+    rate = currencyData[toCurrency]
+    return rate * amount
+
+
 def main():
     try:
-        listCurrency = getListCurrency()
-        print('List of currency convertable\n')
-        for currency in listCurrency:
-            print(currency,end = ' ')
-        print('\n')
-        amount   = int(input('Enter the amount to convert : '))
-        currency = str(input('To which currency (3 letters) : ')).upper()
-        print(convert(amount, currency))
-    except:
-        print('Not a number')
-    
-        
-main()
+        repo = CurrencyRepo(
+            url="https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"
+        )
+        currencyData = repo.getCurrencies()
+        printAvailableCurrencies(list(currencyData.keys()))
+        amount = getUserAmount()
+        if not amount:
+            return
+        currency = getUserCurrency(currencyData.keys())
+        if not currency:
+            return
+        result = convert(amount, currency, currencyData)
+        print(f"{amount:2f} EUR = {result:8f} {currency}")
+
+    except Exception:
+        traceback.print_exc()
+
+
+if __name__ == "__main__":
+    main()
